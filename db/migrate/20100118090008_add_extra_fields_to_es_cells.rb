fbf3de972b522b344fd38b28df7d8ff7386724c4
Sebastien Briois <sb25@mib18858i.dynamic.sanger.ac.uk>
2010-01-18 16:25:54 +0000

    Add columns to ES Cell table

---
+++ db/migrate/20100118090008_add_extra_fields_to_es_cells.rb
@@ -0,0 +1,19 @@
+class AddExtraFieldsToEsCells < ActiveRecord::Migration
+  def self.up
+    add_column    :es_cells, :comment, :string
+    add_column    :es_cells, :contact, :string
+    add_column    :es_cells, :upper_LR_chk_passed, :boolean
+    add_column    :es_cells, :upper_SR_chk_passed, :boolean
+    add_column    :es_cells, :lower_LR_chk_passed, :boolean
+    add_column    :es_cells, :lower_SR_chk_passed, :boolean
+  end
+
+  def self.down
+    remove_column :es_cells, :comment
+    remove_column :es_cells, :contact
+    remove_column :es_cells, :upper_LR_chk_passed
+    remove_column :es_cells, :upper_SR_chk_passed
+    remove_column :es_cells, :lower_LR_chk_passed
+    remove_column :es_cells, :lower_SR_chk_passed
+  end
+end
