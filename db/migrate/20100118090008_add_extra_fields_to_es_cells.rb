f4431b4617d7bd1f9acb1591a8779db9bd200293
Sebastien Briois <sb25@mib18858i.dynamic.sanger.ac.uk>
2010-01-22 10:58:55 +0000

    Move PCR checks from boolean to integer

--- db/migrate/20100118090008_add_extra_fields_to_es_cells.rb
+++ db/migrate/20100118090008_add_extra_fields_to_es_cells.rb
@@ -2,10 +2,10 @@ class AddExtraFieldsToEsCells < ActiveRecord::Migration
   def self.up
     add_column    :es_cells, :comment, :string
     add_column    :es_cells, :contact, :string
-    add_column    :es_cells, :upper_LR_chk_passed, :boolean
-    add_column    :es_cells, :upper_SR_chk_passed, :boolean
-    add_column    :es_cells, :lower_LR_chk_passed, :boolean
-    add_column    :es_cells, :lower_SR_chk_passed, :boolean
+    add_column    :es_cells, :upper_LR_chk_passed, :integer
+    add_column    :es_cells, :upper_SR_chk_passed, :integer
+    add_column    :es_cells, :lower_LR_chk_passed, :integer
+    add_column    :es_cells, :lower_SR_chk_passed, :integer
   end
 
   def self.down
