073849d7c95620bc2b70bc1cd7049ac99fb684a9
Daz Oakley <daz.oakley@gmail.com>
2010-02-05 13:38:00 +0000

    Cleaned up the gem dependency config so we're not looking for gems from github anymore.

--- config/environment.rb
+++ config/environment.rb
@@ -19,14 +19,14 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "thoughtbot-shoulda", :lib => "shoulda/rails", :source => "http://gems.github.com"
-  config.gem 'mislav-will_paginate', :lib => "will_paginate", :source => "http://gems.github.com", :version => '~> 2.3.11'
+  config.gem "shoulda", :lib => "shoulda/rails"
+  config.gem "will_paginate"
   config.gem "authlogic"
   config.gem "searchlogic"
   config.gem "googlecharts", :lib => "gchart"
-  config.gem 'acts_as_audited', :lib => false, :source => 'http://gemcutter.org'
-  config.gem "matthuhiggins-foreigner", :lib => "foreigner", :source => "http://gems.github.com"
-  config.gem "thoughtbot-factory_girl", :lib => "factory_girl", :source => "http://gems.github.com"
+  config.gem 'acts_as_audited', :lib => false
+  config.gem "foreigner"
+  config.gem "factory_girl"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
