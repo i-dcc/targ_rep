5931c4459991f9bdbfbdc2b94e9a462a19404b0d
Daz Oakley <daz.oakley@gmail.com>
2010-02-05 13:38:23 +0000

    Added in some capistrano deployment config.

---
+++ config/deploy/staging.rb
@@ -0,0 +1,13 @@
+set :deploy_to, "/software/team87/brave_new_world/capistrano_managed_apps/staging/#{application}"
+
+namespace :deploy do
+  desc "Symlink shared configs and folders on each release."
+  task :symlink_shared do
+    run "ln -nfs #{shared_path}/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/log /software/team87/brave_new_world/logs/idcc_targ_rep/staging"
+    run "mkdir -m 777 -p /var/tmp/idcc_targ_rep/staging"
+    run "ln -nfs #{shared_path}/tmp /var/tmp/idcc_targ_rep/staging"
+  end
+end
+
+after "deploy:update_code", "deploy:symlink_shared"
