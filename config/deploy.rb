3474e803557d0e9b658d53a7b7a421aad16b63b7
Daz Oakley <daz.oakley@gmail.com>
2010-02-11 09:52:11 +0000

    Fixed a couple of deployment issues

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,8 +22,8 @@ namespace :deploy do
   
   desc "Set the permissions of the filesystem so that others in the team can deploy, and the team87 user can do their stuff"
   task :fix_perms do
-    run "chgrp team87 #{release_path}/tmp"
-    run "chgrp team87 #{release_path}/public"
+    run "chgrp -R team87 #{release_path}/tmp"
+    run "chgrp -R team87 #{release_path}/public"
     run "chmod 02775 #{release_path}"
   end
 end
