32434d12093c213238f1fd4e0cd183f68693757f
Daz Oakley <daz.oakley@gmail.com>
2010-02-25 10:23:53 +0000

    Gave the home page a serious speed boost. [Closes #7]

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,75 @@
 class WelcomeController < ApplicationController
   def index
-    @counts = {}
+    
+    # First fetch our counts from the DB - this direct SQL approach 
+    # is much faster than going via the model....
+    mol_str_counts = molecular_structure_count_by_pipeline()
+    escell_counts  = escell_count_by_pipeline()
+    
+    @pipeline_counts = {}
+    @total_counts    = {
+      :pipelines            => 0,
+      :molecular_structures => 0,
+      :es_cells             => 0
+    }
+    
     Pipeline.all.each do |pipeline|
-      @counts[pipeline.name] = {
-        :molecular_structures   => pipeline.molecular_structure_count,
-        :es_cells               => pipeline.es_cells.count
+      @pipeline_counts[pipeline.name] = {
+        :molecular_structures => mol_str_counts[ pipeline.id ] ? mol_str_counts[ pipeline.id ] : 0,
+        :es_cells             => escell_counts[ pipeline.id ] ? escell_counts[ pipeline.id ] : 0,
       }
+      @total_counts[:pipelines] += 1
+      @total_counts[:molecular_structures] += @pipeline_counts[pipeline.name][:molecular_structures]
+      @total_counts[:es_cells] += @pipeline_counts[pipeline.name][:es_cells]
     end
   end
+  
+  private
+  
+  def molecular_structure_count_by_pipeline
+    sql = <<-SQL
+      select
+        mol_str_counts.id,
+        count( distinct mol_str_counts.molecular_structure_id ) count
+      from 
+        (
+          select distinct pipelines.id, targeting_vectors.molecular_structure_id
+          from pipelines 
+          left join targeting_vectors on pipelines.id = targeting_vectors.pipeline_id
+          union
+          select distinct pipelines.id,	es_cells.molecular_structure_id
+          from pipelines
+          join targeting_vectors on pipelines.id = targeting_vectors.pipeline_id
+          join es_cells on targeting_vectors.id = es_cells.targeting_vector_id
+        ) mol_str_counts
+      group by mol_str_counts.id
+    SQL
+    run_count_sql(sql)
+  end
+  
+  def escell_count_by_pipeline
+    sql = <<-SQL
+      select
+        pipelines.id,
+        count(es_cells.id) count
+      from
+        pipelines
+        join targeting_vectors on pipelines.id = targeting_vectors.pipeline_id
+        join es_cells on targeting_vectors.id = es_cells.targeting_vector_id
+      group by pipelines.id
+    SQL
+    run_count_sql(sql)
+  end
+  
+  def run_count_sql(sql)
+    counts  = {}
+    results = ActiveRecord::Base.connection.execute(sql)
+    
+    results.each_hash do |res|
+      counts[ res["id"].to_i ] = res["count"].to_i
+    end
+    
+    return counts
+  end
+  
 end
