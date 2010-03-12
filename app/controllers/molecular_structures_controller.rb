9bbd9fc96613e4b29797e776e41d42e6506222c5
Sebastien Briois <sb25@mib18858i.dynamic.sanger.ac.uk>
2010-03-12 17:35:30 +0000

    Added advanced search box to the allele page.

--- app/controllers/molecular_structures_controller.rb
+++ app/controllers/molecular_structures_controller.rb
@@ -19,6 +19,7 @@ class MolecularStructuresController < ApplicationController
   # GET /molecular_structures.xml
   # GET /molecular_structures.json
   def index
+    @molecular_structures = @search.all
     @molecular_structures = @molecular_structures.paginate(:page => params[:page])
     
     respond_to do |format|
@@ -63,7 +64,9 @@ class MolecularStructuresController < ApplicationController
     
     respond_to do |format|
       if @molecular_structure.save
+        # Useful for all formats, not only HTML
         update_links_escell_to_targ_vec( @molecular_structure.id, params[:molecular_structure] )
+        
         format.html {
           flash[:notice] = 'Allele successfully created.'
           redirect_to @molecular_structure
@@ -86,8 +89,10 @@ class MolecularStructuresController < ApplicationController
   def update
     respond_to do |format|
       if @molecular_structure.update_attributes(params[:molecular_structure])
+        # Useful for all formats, not only HTML
+        update_links_escell_to_targ_vec( @molecular_structure.id, params[:molecular_structure] )
+        
         format.html {
-          update_links_escell_to_targ_vec( @molecular_structure.id, params[:molecular_structure] )
           flash[:notice] = 'Allele successfully updated.'
           redirect_to @molecular_structure 
         }
@@ -137,30 +142,26 @@ class MolecularStructuresController < ApplicationController
     def find_molecular_structures
       mol_struct_params = params.dup
       
-      # Just keep params that are Molecular Structure attributes.
-      # A molecular structure will be searched from this params
+      # Just keep Molecular Structure params.
       mol_struct_params.delete( "controller" )
       mol_struct_params.delete( "action" )
       mol_struct_params.delete( "format" )
       mol_struct_params.delete( "page" )
       
-      if mol_struct_params.nil? or mol_struct_params.empty?
-        @molecular_structures = MolecularStructure.all
-      else
-        if mol_struct_params[ :loxp_start ] == 'null' and mol_struct_params[ :loxp_end ] == 'null'
-          # If you don't delete these, it will try to search with loxp_start = 0
-          # instead of loxp_start IS NULL
-          mol_struct_params.delete( :loxp_start )
-          mol_struct_params.delete( :loxp_end )
-
-          # Don't break this line in multiple lines or you will get a wrong result
-          search = MolecularStructure.loxp_start_null.loxp_end_null.search( mol_struct_params )
-        else
-          search = MolecularStructure.search( mol_struct_params )
-        end
-
-        @molecular_structures = search.all
+      if mol_struct_params.include? :search
+        mol_struct_params = params[:search]
+      elsif mol_struct_params[ :loxp_start ] == 'null' and mol_struct_params[ :loxp_end ] == 'null'
+        # 'loxp_start_null' and 'loxp_end_null' should be used to force
+        # these fields to be null
+        mol_struct_params.delete( :loxp_start )
+        mol_struct_params.delete( :loxp_end )
+        mol_struct_params.update({
+          :loxp_start_null  => true,
+          :loxp_end_null    => true
+        })
       end
+      
+      @search = MolecularStructure.search( mol_struct_params )
     end
     
     def format_nested_params
@@ -245,13 +246,13 @@ class MolecularStructuresController < ApplicationController
         end
       end
     end
-  
-    # When trying to create a new targeting vector and link it at the
-    # same time to an ES Cell via its name, it won't work properly.
-    # Following is a fix to this problem - specific to HTML pages
+    
+    # One can give a targeting_vector_name instead of a targeting_vector_id
+    # to link an ES Cell to its Targeting Vector.
+    # This function will find the right targeting vector from the given name
     def update_links_escell_to_targ_vec( mol_struct_id, params )
       return unless params.include? :es_cells_attributes
-    
+      
       es_cells_attrs = []
       if params[:es_cells_attributes].is_a? Array
         params[:es_cells_attributes].each do |attrs|
@@ -265,19 +266,19 @@ class MolecularStructuresController < ApplicationController
       
       es_cells_attrs.each do |attrs|
         if attrs.include? :targeting_vector_name
-          # Find ES Cell
+          
+          # Find ES Cell from its 'id' or its 'name' + 'molecular_structure_id'
           if attrs.include? :id
-            es_cell = EsCell.find attrs[:id]
+            es_cell = EsCell.find( attrs[:id] )
           else
-            search = EsCell.search
-            search.name_like = attrs[:name]
-            search.molecular_structure_id_is = mol_struct_id
+            search = EsCell.search({
+              :name_like                 => attrs[:name],
+              :molecular_structure_id_is => mol_struct_id
+            })
             es_cell = search.first
           end
-        
-          # If ES Cell is not related to a targeting vector though a
-          # targeting vector name has been provided, let's find the targ
-          # vector and link it to the ES Cell
+          
+          # Find targeting vector from given name and link it to the ES Cell
           if es_cell and es_cell.targeting_vector.nil?
             search = TargetingVector.name_is(attrs[:targeting_vector_name])
             es_cell.targeting_vector = search.first
