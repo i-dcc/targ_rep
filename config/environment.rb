466df37d6c002878556750e23b54b4de29ef0bfc
Daz Oakley <daz.oakley@gmail.com>
2010-03-17 14:01:09 +0000

    Added NewRelic RPM into the app to track our performance.

--- config/environment.rb
+++ config/environment.rb
@@ -25,6 +25,8 @@ Rails::Initializer.run do |config|
   config.gem "googlecharts", :lib => "gchart"
   config.gem 'acts_as_audited', :lib => false
   config.gem "matthuhiggins-foreigner", :lib => "foreigner"
+  
+  config.gem "newrelic_rpm"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
