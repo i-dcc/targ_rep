97812342010234d60293173ba68ff807a2de7f19
Daz Oakley <do2@sanger.ac.uk>
2010-07-26 13:38:01 +0100

    Initial commit of seed data for the 'qc_field_descriptions' table - need Ed to give me data to complete. refs #1206

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,71 @@ pipelines = Pipeline.create([
   { :name => 'EUCOMM' },
   { :name => 'mirKO' }
 ])
+
+qc_field_descriptions = QcFieldDescriptions.create([
+  {
+    :qc_field    => 'qc_five_prime_lr_pcr',
+    :description => "Long Range PCR (LR-PCR) test between the selection cassette and beyond the 5' homology arm.",
+    :url         => ''
+  },
+  {
+    :qc_field    => 'qc_three_prime_lr_pcr',
+    :description => "Long Range PCR (LR-PCR) test between the selection cassette and beyond the 3' homology arm.",
+    :url         => ''
+  },
+  {
+    :qc_field    => 'qc_map_test',
+    :description => "",
+    :url         => ''
+  },
+  {
+    :qc_field    => 'qc_karyotype',
+    :description => "",
+    :url         => ''
+  },
+  {
+    :qc_field    => 'qc_tv_backbone_assay',
+    :description => "",
+    :url         => ''
+  },
+  {
+    :qc_field    => 'qc_loxp_confirmation',
+    :description => "",
+    :url         => ''
+  },
+  {
+    :qc_field    => 'qc_southern_blot',
+    :description => "",
+    :url         => ''
+  },
+  {
+    :qc_field    => 'qc_loss_of_wt_allele',
+    :description => "",
+    :url         => ''
+  },
+  {
+    :qc_field    => 'qc_neo_count_qpcr',
+    :description => "",
+    :url         => ''
+  },
+  {
+    :qc_field    => 'qc_lacz_sr_pcr',
+    :description => "",
+    :url         => ''
+  },
+  {
+    :qc_field    => 'qc_mutant_specific_sr_pcr',
+    :description => "",
+    :url         => ''
+  },
+  {
+    :qc_field    => 'qc_five_prime_cassette_integrity',
+    :description => "",
+    :url         => ''
+  },
+  {
+    :qc_field    => 'qc_neo_sr_pcr',
+    :description => "",
+    :url         => ''
+  }
+])
