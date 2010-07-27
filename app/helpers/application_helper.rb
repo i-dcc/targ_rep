c8ddd883057141b37340949c1e7da46c9551bb53
Daz Oakley <do2@sanger.ac.uk>
2010-07-27 16:42:14 +0100

    interface tidy. refs #1206

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  def print_dash_on_nil_or_empty( arg )
+    if [nil,''].include?(arg)
+      return '-'
+    else
+      return arg
+    end
+  end
+  
 end
