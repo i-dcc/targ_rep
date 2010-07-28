6b938d5f27bf448e1efd896e85a9dad5fac42614
Daz Oakley <do2@sanger.ac.uk>
2010-07-28 10:26:44 +0100

    More interface cleaning. refs #1206

--- app/helpers/molecular_structures_helper.rb
+++ app/helpers/molecular_structures_helper.rb
@@ -4,7 +4,7 @@ module MolecularStructuresHelper
   
   # Add a new es cell form
   def add_es_cell_link(form_builder)
-    link_to_function 'Add an ES Cell' do |page|
+    link_to_function( 'Add an ES Cell', { :class => 'ss_sprite ss_add' } ) do |page|
       form_builder.fields_for :es_cells, EsCell.new, :child_index => 'NEW_RECORD' do |f|
         html = render( :partial => 'es_cell', :locals => { :f => f } )
         page << "$('es_cells').insert({ bottom: '#{escape_javascript(html)}'.replace(/NEW_RECORD/g, new Date().getTime()) });"
@@ -16,7 +16,7 @@ module MolecularStructuresHelper
   def remove_es_cell_link(form_builder)
     if form_builder.object.new_record?
       # If the es cell is a new record, we can just remove from the dom
-      link_to_function("remove", nil) do |page|
+      link_to_function( "remove", { :class => 'ss_sprite ss_cross' } ) do |page|
         page << "$(this).up('tr.es_cell').next('tr.es_cell_qc').remove();"
         page << "$(this).up('tr.es_cell').remove();"
       end
@@ -25,10 +25,10 @@ module MolecularStructuresHelper
       # for this reason the new fields_for, accept_nested_attributes helpers give us _destroy,
       # a virtual attribute that tells rails to delete the child record.
       form_elm = form_builder.hidden_field(:_destroy)
-      link_elm = link_to_function("remove", nil) do |page|
+      link_elm = link_to_function("remove", { :class => 'ss_sprite ss_cross' } ) do |page|
         page << "$(this).up('tr.es_cell').next('tr.es_cell_qc').hide();"
         page << "$(this).up('tr.es_cell').hide();"
-        page << "$(this).previous().value = '1'"
+        page << "$(this).previous().value = '1';"
       end
       
       return form_elm + link_elm
@@ -37,7 +37,7 @@ module MolecularStructuresHelper
   
   # Add a new targeting vector form
   def add_targ_vec_link(form_builder)
-    link_to_function 'Add a targeting vector' do |page|
+    link_to_function( 'Add a targeting vector', { :class => 'ss_sprite ss_add' } ) do |page|
       form_builder.fields_for :targeting_vectors, TargetingVector.new, :child_index => 'NEW_RECORD' do |f|
         html = render :partial => 'targ_vec', :locals => { :f => f } 
         page << "$('targeting_vectors').insert({ bottom: '#{escape_javascript(html)}'.replace(/NEW_RECORD/g, new Date().getTime()) });"
@@ -49,13 +49,17 @@ module MolecularStructuresHelper
   def remove_targ_vec_link(form_builder)
     if form_builder.object.new_record?
       # If the targeting vector is a new record, we can just remove the div from the dom
-      link_to_function("remove", "$(this).up('.targeting_vector').remove();");
+      link_to_function( "remove", { :class => 'ss_sprite ss_cross' } ) do |page|
+        page << "$(this).up('.targeting_vector').remove();"
+      end
     else
       # However if it's a "real" record it has to be deleted from the database,
       # for this reason the new fields_for, accept_nested_attributes helpers give us _destroy,
       # a virtual attribute that tells rails to delete the child record.
       form_builder.hidden_field(:_destroy) +
-      link_to_function("remove", "$(this).up('.targeting_vector').hide(); $(this).previous().value = '1'")
+      link_to_function( "remove", { :class => 'ss_sprite ss_cross' } ) do |page|
+        page << "$(this).up('.targeting_vector').hide(); $(this).previous().value = '1';"
+      end
     end
   end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,15 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+document.observe('dom:loaded', function() {
+  
+  // Toggles to show/hide the ES Cell QC metrics
+  $$('a.es_cell_qc_toggle').each( function(link) {
+    link.observe( 'click', function(event) {
+      event.stop();
+      $(this).up('tr').next('tr.es_cell_qc').toggle();
+    });
+  });
+  
+});
+
