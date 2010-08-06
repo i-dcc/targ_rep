0920b6ae38dcabb171f770d55646d4dbb6b39b19
Daz Oakley <do2@sanger.ac.uk>
2010-08-06 09:49:25 +0100

    Small updates to the ES Cell QC

--- config/initializers/es_cell_qc_options.rb
+++ config/initializers/es_cell_qc_options.rb
@@ -3,15 +3,15 @@
 # options for the ESCell QC metrics in a single location...
 
 qc_metrics = {
-  :production_qc_five_prime_screen => ["pass","fail","not applicable"],
+  :production_qc_five_prime_screen => ["pass","fail","no reads detected","not attempted"],
+  :production_qc_three_prime_screen => ["pass","fail","no reads detected"],
+  :production_qc_loxp_screen => ["pass","fail","no reads detected"],
   
   :user_qc_karyotype     => ["pass","fail","limit"],
   :user_qc_southern_blot => ["pass","fail 5' end","fail 3' end","fail both ends","double integration"]
 }
 
 pass_fail_only_qc_fields = [
-  :production_qc_three_prime_screen,
-  :production_qc_loxp_screen,
   :production_qc_loss_of_allele,
   :production_qc_vector_integrity,
   
