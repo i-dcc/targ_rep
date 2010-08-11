bc8ca672a9fc3dd2fb50679aefbfdbf2e55dc3ad
Daz Oakley <do2@sanger.ac.uk>
2010-08-11 15:11:06 +0100

    corrected the partial names in the alleles_helper. refs #1266

--- app/helpers/alleles_helper.rb
+++ app/helpers/alleles_helper.rb
@@ -6,7 +6,7 @@ module AllelesHelper
   def add_es_cell_link(form_builder)
     link_to_function( 'Add an ES Cell', { :class => 'ss_sprite ss_add' } ) do |page|
       form_builder.fields_for :es_cells, EsCell.new, :child_index => 'NEW_RECORD' do |f|
-        html = render( :partial => 'es_cell', :locals => { :f => f } )
+        html = render( :partial => 'es_cell_form', :locals => { :f => f } )
         page << "$('es_cells').insert({ bottom: '#{escape_javascript(html)}'.replace(/NEW_RECORD/g, new Date().getTime()) });"
       end
     end
@@ -39,7 +39,7 @@ module AllelesHelper
   def add_targ_vec_link(form_builder)
     link_to_function( 'Add a targeting vector', { :class => 'ss_sprite ss_add' } ) do |page|
       form_builder.fields_for :targeting_vectors, TargetingVector.new, :child_index => 'NEW_RECORD' do |f|
-        html = render :partial => 'targ_vec', :locals => { :f => f } 
+        html = render :partial => 'targ_vec_form', :locals => { :f => f } 
         page << "$('targeting_vectors').insert({ bottom: '#{escape_javascript(html)}'.replace(/NEW_RECORD/g, new Date().getTime()) });"
       end
     end
