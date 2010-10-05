15fc2c5d5eb379df2ea78e43dbcd6dc778b9244c
Daz Oakley <do2@sanger.ac.uk>
2010-10-05 18:44:30 +0100

    Added Errbit reporting conf.

--- config/environment.rb
+++ config/environment.rb
@@ -27,6 +27,7 @@ Rails::Initializer.run do |config|
   config.gem "allele_image"
   config.gem "rsolr"
   config.gem "newrelic_rpm"
+  config.gem 'hoptoad_notifier'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
---
+++ config/initializers/errbit.rb
@@ -0,0 +1,5 @@
+HoptoadNotifier.configure do |config|
+   config.api_key = '531e54126217ae9a5d350adf22c30375'
+   config.host    = 'htgt.internal.sanger.ac.uk'
+   config.port    = 4007 # Note: Deployment notifications only work on port 80
+end
