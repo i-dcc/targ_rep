3a1f4db5f2d39fc890dd367608a5fa8da0cdfa2f
Daz Oakley <do2@sanger.ac.uk>
2010-11-28 16:57:37 +0000

    Update rails to 2.3.10

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.10' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -19,7 +19,7 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "will_paginate", :version => '2.3.15'
+  config.gem "will_paginate"
   config.gem "authlogic"
   config.gem "searchlogic"
   config.gem "acts_as_audited", :lib => false
