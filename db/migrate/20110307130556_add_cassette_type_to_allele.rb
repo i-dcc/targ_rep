c11dac1e34a9f476116ac8b988791ece0d8806b5
Daz Oakley <do2@sanger.ac.uk>
2011-03-24 11:05:27 +0000

    Restrict the cassette_type database field to 50 characters.

--- db/migrate/20110307130556_add_cassette_type_to_allele.rb
+++ db/migrate/20110307130556_add_cassette_type_to_allele.rb
@@ -1,6 +1,6 @@
 class AddCassetteTypeToAllele < ActiveRecord::Migration
   def self.up
-    add_column :alleles, :cassette_type, :string
+    add_column :alleles, :cassette_type, :string, :limit => 50
   end
 
   def self.down
