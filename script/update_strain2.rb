b3b424e5b33d6577e8b38500b47c9045c66abb5c
Richard Easty <re4@sanger.ac.uk>
2012-08-22 14:03:09 +0100

    Repair wrong cell strain for all Regeneron Alleles (es cell strain output via biomart)

--- script/update_strain2.rb
+++ script/update_strain2.rb
@@ -1,9 +1,25 @@
-# Get all es_cells and set the strain...
+#Bug #8753
+#Repair wrong cell strain for all Regeneron Alleles (es cell strain output via biomart)
+#
+#Added by Vivek Iyer 21 days ago. Updated 6 days ago.
+#
+#Description
+#
+#All regeneron alleles currently have parental_cell_line => VGB6
+#
+#The targrep biomart is reporting an additional field: ES Cell Strain for these alleles:
+#es_cell_strain => VGB6,
+#
+#but this is wrong.
+#
+#The cell line is VGB6, the cell strain should be C57Bl/6N - I'm not sure where how information is getting into the mart
+#(either from a hidden / dependent field in the targrep itself, or from the martbuild process).
 
-COUNT = 14490
+COUNT = 16570
 DEBUG = false
 
 puts "Environment: #{Rails.env}"
+puts "DEBUG" if DEBUG
 
 EsCell.transaction do
   counter = 0
