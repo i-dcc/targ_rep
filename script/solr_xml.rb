582c4eff547d98e1d60db9553ab7af2edd845dda
Richard Easty <re4@sanger.ac.uk>
2012-08-23 10:05:40 +0100

    add schema gen

--- script/solr_xml.rb
+++ script/solr_xml.rb
@@ -1,49 +1,87 @@
 
-require 'pp'
-
-xml_str = ''
-xml = []
-xml.push '<add>'
+#require 'pp'
 
 es_cell_attributes = %w(allele_id name allele_symbol_superscript ikmc_project_id mgi_allele_id parental_cell_line strain)
+cdata_attributes = %w(allele_symbol_superscript strain)
+break_count = 100
 
-count = 0
+def build_index
+  xml_str = ''
+  xml = []
+  xml.push '<add>'
 
-EsCell.find_each( :batch_size => 500 ) do |cell|
+  count = 0
 
-  xml.push '<doc>'
+  EsCell.find_each( :batch_size => 500 ) do |cell|
 
-  #  xml.push "<field name='name'>#{cell.name}</field>"
+    xml.push '<doc>'
 
-  #cell.attributes.each do |attribute|
-  #  xml.push "<field name='#{attribute[0]}'>#{attribute[1]}</field>"
-  #  pp attribute
-  #end
+    es_cell_attributes.each do |attribute|
+      attribute_name = attribute == 'name' ? 'es_cell_name' : attribute
+      xml.push "<field name='#{attribute_name}'><![CDATA[#{cell.send(attribute)}]]></field>" if cdata_attributes.include? attribute_name
+      xml.push "<field name='#{attribute_name}'>#{cell.send(attribute)}</field>" if ! cdata_attributes.include? attribute_name
+    end
 
-  es_cell_attributes.each do |attribute|
-    attribute_name = attribute == 'name' ? 'es_cell_name' : attribute
-    xml.push "<field name='#{attribute_name}'><![CDATA[#{cell.send(attribute)}]]></field>" if attribute_name == 'allele_symbol_superscript' || attribute_name == 'strain'
-    xml.push "<field name='#{attribute_name}'>#{cell.send(attribute)}</field>" if attribute_name != 'allele_symbol_superscript' && attribute_name != 'strain'
+    xml.push "<field name='mutation_subtype'>#{cell.allele.mutation_subtype}</field>"
+
+    xml.push '</doc>'
+
+    count += 1
+
+    break if break_count > -1 && count >= break_count
   end
 
-  xml.push "<field name='mutation_subtype'>#{cell.allele.mutation_subtype}</field>"
+  xml.push '</add>'
 
-  xml.push '</doc>'
+  xml_str += xml.join("\n")
 
-  count += 1
+  puts File.dirname(__FILE__)
+  puts Dir.pwd + '/targ_rep.xml'
 
-  break if count > 100
+  target_file = File.dirname(__FILE__) + '/targ_rep.xml'
+
+  File.open(target_file, 'w') {|f| f.write(xml_str) }
+
+  puts "done!"
 end
 
-xml.push '</add>'
+def dump_attributes(model)
+  model.attributes.each do |attribute|
+    xml.push "<field name='#{attribute[0]}'>#{attribute[1]}</field>"
+  end
+end
 
-xml_str += xml.join("\n")
+def create_schema_xml(all_attributes)
+  replacement_text = []
+  all_attributes.each do |attribute|
+    replacement_text.push("<field name='#{attribute}' type='text' indexed='true' stored='true' />")
+  end
 
-puts File.dirname(__FILE__)
-puts Dir.pwd + '/targ_rep.xml'
+  replacement_text = replacement_text.join "\n"
 
-target_file = File.dirname(__FILE__) + '/targ_rep.xml'
+  replacement_text2 = []
+  all_attributes.each do |attribute|
+    replacement_text2.push("<copyField source='#{attribute}' dest='text' />")
+  end
 
-File.open(target_file, 'w') {|f| f.write(xml_str) }
+  replacement_text2 = replacement_text2.join "\n"
 
-puts "done!"
+  text = IO.read(File.dirname(__FILE__) + '/schema.xml')
+
+  text = text.sub(/\<\!\-\-TARG_REP_FIELD_START\-\-\>(.+?)\<\!\-\-TARG_REP_FIELD_END\-\-\>/m, '<!--TARG_REP_FIELD_START-->' + "\n#{replacement_text}\n" + '<!--TARG_REP_FIELD_END-->')
+
+  text = text.sub(/\<\!\-\-TARG_REP_COPYFIELD_BEGIN\-\-\>(.+?)\<\!\-\-TARG_REP_COPYFIELD_END\-\-\>/m, '<!--TARG_REP_COPYFIELD_BEGIN-->' + "\n#{replacement_text2}\n" + '<!--TARG_REP_COPYFIELD_END-->')
+
+
+#<uniqueKey>mgi_allele_id</uniqueKey>
+
+
+
+
+
+  IO.write(File.dirname(__FILE__) + '/schema_new.xml', text)
+end
+
+################################################################################
+
+create_schema_xml(es_cell_attributes + cdata_attributes)
