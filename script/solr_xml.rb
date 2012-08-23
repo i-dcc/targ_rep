94aa986b4956b49cbb919e1337bf92d05eacddc8
Richard Easty <re4@sanger.ac.uk>
2012-08-23 14:28:25 +0100

    tidy

--- script/solr_xml.rb
+++ script/solr_xml.rb
@@ -1,9 +1,9 @@
 
-#require 'pp'
-
-es_cell_attributes = %w(allele_id name allele_symbol_superscript ikmc_project_id mgi_allele_id parental_cell_line strain)
-cdata_attributes = %w(allele_symbol_superscript strain)
-break_count = 100
+$es_cell_attributes = %w(allele_id name allele_symbol_superscript ikmc_project_id mgi_allele_id parental_cell_line strain)
+$cdata_attributes = %w(allele_symbol_superscript strain)
+$all_attributes = $es_cell_attributes + $cdata_attributes
+$unique_key = 'mgi_allele_id'
+$break_count = -1
 
 def build_index
   xml_str = ''
@@ -16,10 +16,10 @@ def build_index
 
     xml.push '<doc>'
 
-    es_cell_attributes.each do |attribute|
+    $es_cell_attributes.each do |attribute|
       attribute_name = attribute == 'name' ? 'es_cell_name' : attribute
-      xml.push "<field name='#{attribute_name}'><![CDATA[#{cell.send(attribute)}]]></field>" if cdata_attributes.include? attribute_name
-      xml.push "<field name='#{attribute_name}'>#{cell.send(attribute)}</field>" if ! cdata_attributes.include? attribute_name
+      xml.push "<field name='#{attribute_name}'><![CDATA[#{cell.send(attribute)}]]></field>" if $cdata_attributes.include? attribute_name
+      xml.push "<field name='#{attribute_name}'>#{cell.send(attribute)}</field>" if ! $cdata_attributes.include? attribute_name
     end
 
     xml.push "<field name='mutation_subtype'>#{cell.allele.mutation_subtype}</field>"
@@ -28,7 +28,7 @@ def build_index
 
     count += 1
 
-    break if break_count > -1 && count >= break_count
+    break if $break_count > -1 && count >= $break_count
   end
 
   xml.push '</add>'
@@ -51,16 +51,16 @@ def dump_attributes(model)
   end
 end
 
-def create_schema_xml(all_attributes)
+def create_schema_xml
   replacement_text = []
-  all_attributes.each do |attribute|
+  $all_attributes.each do |attribute|
     replacement_text.push("<field name='#{attribute}' type='text' indexed='true' stored='true' />")
   end
 
   replacement_text = replacement_text.join "\n"
 
   replacement_text2 = []
-  all_attributes.each do |attribute|
+  $all_attributes.each do |attribute|
     replacement_text2.push("<copyField source='#{attribute}' dest='text' />")
   end
 
@@ -72,16 +72,13 @@ def create_schema_xml(all_attributes)
 
   text = text.sub(/\<\!\-\-TARG_REP_COPYFIELD_BEGIN\-\-\>(.+?)\<\!\-\-TARG_REP_COPYFIELD_END\-\-\>/m, '<!--TARG_REP_COPYFIELD_BEGIN-->' + "\n#{replacement_text2}\n" + '<!--TARG_REP_COPYFIELD_END-->')
 
+  text = text.sub(/\<uniqueKey\>(.+?)\<\/uniqueKey\>/m, '<uniqueKey>' + "#{$unique_key}" + '</uniqueKey>')
 
-#<uniqueKey>mgi_allele_id</uniqueKey>
-
-
-
-
-
-  IO.write(File.dirname(__FILE__) + '/schema_new.xml', text)
+  IO.write(File.dirname(__FILE__) + '/schema.xml', text)
 end
 
 ################################################################################
 
-create_schema_xml(es_cell_attributes + cdata_attributes)
+create_schema_xml
+
+build_index
