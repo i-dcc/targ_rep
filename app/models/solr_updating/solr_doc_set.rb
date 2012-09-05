6214d6a96eb6f1a547015fb6e51ffe9920ecbb9a
Asfand Qazi <aq2@sanger.ac.uk>
2012-09-05 13:32:53 +0100

    Mocked up implementation of IndexUpdateQueue

---
+++ app/models/solr_updating/solr_doc_set.rb
@@ -0,0 +1,2 @@
+class SolrUpdating::SolrDocSet
+end
---
+++ test/unit/solr_updating/index_update_queue_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class SolrUpdating::IndexUpdateQueueTest < ActiveSupport::TestCase
+  context 'SolrUpdating::IndexUpdateQueue' do
+
+    should 'add new doc sets to beginning of the queue and retrieve them from the end' do
+      doc_set_1 = stub('doc_set_1')
+      doc_set_2 = stub('doc_set_2')
+      seq1 = sequence('seq1')
+
+      SolrUpdating::SolrDocSet.expects(:add).with(doc_set_1).in_sequence(seq1)
+      SolrUpdating::SolrDocSet.expects(:add).with(doc_set_2).in_sequence(seq1)
+
+      SolrUpdating::IndexUpdateQueue.add(doc_set_1)
+      SolrUpdating::IndexUpdateQueue.add(doc_set_2)
+
+      SolrUpdating::SolrDocSet.expects(:earliest).returns(doc_set_1).in_sequence(seq1)
+      doc_set_1.expects(:do_this_before_destroy).in_sequence(seq1)
+      doc_set_1.expects(:destroy).with().in_sequence(seq1)
+
+      SolrUpdating::IndexUpdateQueue.remove_safely { |ds| ds.do_this_before_destroy }
+    end
+
+  end
+end
