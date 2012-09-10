3a52eb7cc6b88db3d99c2b719cf101e0c4017f8a
Asfand Qazi <aq2@sanger.ac.uk>
2012-09-10 15:07:00 +0100

    Add SolrCommand.earliest_first

--- app/models/solr_update/solr_command.rb
+++ app/models/solr_update/solr_command.rb
@@ -1,5 +1,9 @@
 class SolrUpdate::SolrCommand < ActiveRecord::Base
   set_table_name :solr_updating_solr_commands
+
+  def self.earliest_first
+    return self.find(:all, :order => 'created_at asc')
+  end
 end
 
 # == Schema Information
--- test/unit/solr_update/solr_command_test.rb
+++ test/unit/solr_update/solr_command_test.rb
@@ -5,5 +5,14 @@ class SolrUpdate::SolrCommandTest < ActiveSupport::TestCase
 
     should have_db_column(:data).with_options(:null => false)
 
+    should 'return commands in earliest first order with #earliest_first' do
+      SolrUpdate::SolrCommand.create!(:data => 'data2', :created_at => '2012-01-02 00:00:00 UTC')
+      SolrUpdate::SolrCommand.create!(:data => 'data1', :created_at => '2012-01-01 00:00:00 UTC')
+      SolrUpdate::SolrCommand.create!(:data => 'data3', :created_at => '2012-01-03 00:00:00 UTC')
+
+      data_values = SolrUpdate::SolrCommand.earliest_first.map(&:data)
+      assert_equal ['data1', 'data2', 'data3'], data_values
+    end
+
   end
 end
