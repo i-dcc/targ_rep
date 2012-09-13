0279f057b1bb33e59bcbf66b7a387540ffb49b47
Asfand Qazi <aq2@sanger.ac.uk>
2012-09-13 12:28:09 +0100

    Some more work on the SolrCommandFactory

--- app/models/solr_update/solr_command_factory.rb
+++ app/models/solr_update/solr_command_factory.rb
@@ -1,11 +1,35 @@
 class SolrUpdate::SolrCommandFactory
+
   def self.create_solr_command(allele)
+    factory = self.new(allele)
+    return factory.create_solr_command
+  end
+
+  def create_solr_command
     commands = ActiveSupport::OrderedHash.new
 
+    docs = allele.es_cells.unique_solr_info.map do |es_cell_info|
+      {
+        'type' => 'allele',
+        'id' => allele.id,
+        'allele_type' => formatted_allele_type
+      }
+    end
+
     commands['delete'] = {'query' => "type:allele AND id:#{allele.id}"}
-    commands['add'] = ''
+    commands['add'] = docs
     commands['commit'] = ''
 
     return commands.to_json
+  end
+
+  attr_reader :allele
+
+  def initialize(allele)
+    @allele = allele
+  end
+
+  def formatted_allele_type
+    return allele.mutation_subtype.titleize
   end
 end
--- test/unit/solr_update/solr_command_factory_test.rb
+++ test/unit/solr_update/solr_command_factory_test.rb
@@ -2,13 +2,23 @@ require 'test_helper'
 
 class SolrUpdate::SolrCommandFactoryTest < ActiveSupport::TestCase
   context 'SolrUpdate::SolrCommandFactory' do
+    should 'format allele type from allele mutation_subtype: #formatted_allele_type' do
+      allele = Factory.build :allele
+      factory = SolrUpdate::SolrCommandFactory.new(allele)
+
+      allele.mutation_subtype = 'deletion'
+      assert_equal 'Deletion', factory.__send__(:formatted_allele_type)
+
+      allele.mutation_subtype = 'targeted_non_conditional'
+      assert_equal 'Targeted Non Conditional', factory.__send__(:formatted_allele_type)
+    end
+
     context 'when creating solr command for an allele' do
 
       setup do
         SolrUpdate::IndexProxy::Gene.stubs(:get_marker_symbol).with('MGI:9999999991').returns('Test1')
 
-        # eucomm = Pipeline.find_or_create_by_name('EUCOMM')
-        @allele = Factory.create :allele, :mutation_subtype => 'conditional_ready',
+        @allele = Factory.create :allele, :design_type => 'Knock Out',
                 :mgi_accession_id => 'MGI:9999999991'
 
         fake_unique_solr_info = [
@@ -18,11 +28,6 @@ class SolrUpdate::SolrCommandFactoryTest < ActiveSupport::TestCase
 
         @allele.es_cells.stubs(:unique_solr_info).returns(fake_unique_solr_info)
 
-        # @es_cell1 = Factory.create :es_cell, :allele => @allele, :parental_cell_line => 'VGB6', :pipeline => eucomm
-        # @es_cell2 = Factory.create :es_cell, :allele => @allele, :parental_cell_line => 'JM8A3.N1', :pipeline => eucomm
-        # assert_equal 'C57BL/6N', @es_cell1.strain
-        # assert_equal 'C57BL/6N-A<tm1Brd>/a', @es_cell2.strain
-
         @commands_json = SolrUpdate::SolrCommandFactory.create_solr_command(@allele)
         @commands = JSON.parse(@commands_json, :object_class => ActiveSupport::OrderedHash)
       end
@@ -31,18 +36,22 @@ class SolrUpdate::SolrCommandFactoryTest < ActiveSupport::TestCase
         assert_equal %w{delete add commit}, @commands.keys
       end
 
-      should 'have delete all docs for tha allele' do
+      should 'delete all docs for tha allele before adding them' do
         assert_equal "type:allele AND id:#{@allele.id}", @commands['delete']['query']
       end
 
-      context 'in the docs being added to the index' do
+      context 'the add commands' do
         should 'set allele id' do
           assert_equal [@allele.id, @allele.id], @commands['add'].map {|d| d['id']}
         end
 
-        should 'set type'
+        should 'set type' do
+          assert_equal ['allele', 'allele'], @commands['add'].map {|d| d['type']}
+        end
 
-        should 'set allele_type'
+        should 'set allele_type' do
+          assert_equal ['Conditional Ready', 'Conditional Ready'], @commands['add'].map {|d| d['allele_type']}
+        end
 
         should 'set strain'
       end
