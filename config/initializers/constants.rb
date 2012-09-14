7c2c6cb37ad060af5198d1b0ba9b59e9be17a2d8
Peter Matthews <pm9@sanger.ac.uk>
2012-09-14 16:29:43 +0100

    use perl script if available otherwise use mock

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,6 +1,12 @@
-GENBANK_RECOMBINATION_SCRIPT_PATH = case Rails.env
-when 'production' then
-  'recombinate_sequence.pl'
+if Rails.env == 'production'
+  GENBANK_RECOMBINATION_SCRIPT_PATH = 'recombinate_sequence.pl'
 else
-  'test/lib/recombinate_sequence.pl'
+  script_path = 'test/lib/recombinate_sequence.pl'
+  ENV['PATH'].split(':').each do |folder|
+    if File.exists?("#{folder}/recombinate_sequence.pl")
+      script_path = 'recombinate_sequence.pl'
+      break
+    end
+  end
+  GENBANK_RECOMBINATION_SCRIPT_PATH = script_path
 end
