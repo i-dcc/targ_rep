22144f5dec8c236af822fa178bc4e6b21aa2d5b9
Asfand Qazi <aq2@sanger.ac.uk>
2012-09-19 15:08:19 +0100

    Rearchitect queue to build solr update command packet AFTER queue runs

--- app/models/solr_update/queue.rb
+++ app/models/solr_update/queue.rb
@@ -6,4 +6,17 @@ class SolrUpdate::Queue
   def self.enqueue_for_delete(object)
     SolrUpdate::Queue::Item.add(object, 'delete')
   end
+
+  def self.run
+    p = SolrUpdate::IndexProxy::Allele.new
+    SolrUpdate::Queue::Item.process_in_order do |object, command_type|
+      if command_type == 'update'
+        command = SolrUpdate::CommandFactory.create_solr_command_to_update_in_index(object)
+      elsif command_type == 'delete'
+        command = SolrUpdate::CommandFactory.create_solr_command_to_delete_from_index(object)
+      end
+      p.update(command)
+    end
+  end
+
 end
--- test/unit/solr_update/queue_test.rb
+++ test/unit/solr_update/queue_test.rb
@@ -4,19 +4,37 @@ class SolrUpdate::QueueTest < ActiveSupport::TestCase
   context 'SolrUpdate::Queue' do
 
     should 'allow adding of queue items' do
-      allele1 = Factory.build(:allele).stubs(:id => 1)
-      allele2 = Factory.build(:allele).stubs(:id => 2)
-      allele3 = Factory.build(:allele).stubs(:id => 3)
+      object1 = stub('object1')
+      object2 = stub('object2')
+      object3 = stub('object3')
 
       seq = sequence('seq')
 
-      SolrUpdate::Queue::Item.expects(:add).with(allele1, 'update').in_sequence(seq)
-      SolrUpdate::Queue::Item.expects(:add).with(allele2, 'update').in_sequence(seq)
-      SolrUpdate::Queue::Item.expects(:add).with(allele3, 'delete').in_sequence(seq)
+      SolrUpdate::Queue::Item.expects(:add).with(object1, 'update').in_sequence(seq)
+      SolrUpdate::Queue::Item.expects(:add).with(object2, 'update').in_sequence(seq)
+      SolrUpdate::Queue::Item.expects(:add).with(object3, 'delete').in_sequence(seq)
 
-      SolrUpdate::Queue.enqueue_for_update(allele1)
-      SolrUpdate::Queue.enqueue_for_update(allele2)
-      SolrUpdate::Queue.enqueue_for_delete(allele3)
+      SolrUpdate::Queue.enqueue_for_update(object1)
+      SolrUpdate::Queue.enqueue_for_update(object2)
+      SolrUpdate::Queue.enqueue_for_delete(object3)
+    end
+
+    should 'be run and process items in order they were added' do
+      object1 = stub('object1')
+      object2 = stub('object2')
+
+      command1 = stub('command1')
+      command2 = stub('command2')
+
+      SolrUpdate::Queue::Item.expects(:process_in_order).with().multiple_yields([object1, 'update'], [object2, 'delete'])
+
+      SolrUpdate::CommandFactory.expects(:create_solr_command_to_update_in_index).with(object1).returns(command1)
+      SolrUpdate::CommandFactory.expects(:create_solr_command_to_delete_from_index).with(object2).returns(command2)
+
+      SolrUpdate::IndexProxy::Allele.any_instance.expects(:update).with(command1)
+      SolrUpdate::IndexProxy::Allele.any_instance.expects(:update).with(command2)
+
+      SolrUpdate::Queue.run
     end
 
   end
