49cf14937e554aa106c81444f6ca3cec7c7a1e0c
Richard Easty <re4@sanger.ac.uk>
2012-10-10 14:58:41 +0100

    Remove commented code.

--- db/migrate/20120921103010_create_distribution_qcs.rb
+++ db/migrate/20120921103010_create_distribution_qcs.rb
@@ -24,21 +24,6 @@ class CreateDistributionQcs < ActiveRecord::Migration
       t.timestamps
     end
 
-# TODO: remove me!
-    Centre.create!(:id => 1, :name => "WTSI") if ! Centre.find_by_name "WTSI"
-    Centre.create!(:id => 2, :name => "KOMP") if ! Centre.find_by_name "KOMP"
-    Centre.create!(:id => 3, :name => "EUCOMM") if ! Centre.find_by_name "EUCOMM"
-
-    #execute "
-    #  insert into distribution_qcs (five_prime_sr_pcr, three_prime_sr_pcr, karyotype_low, karyotype_high, copy_number, five_prime_lr_pcr, three_prime_lr_pcr,
-    #          thawing, loa, loxp, lacz, chr1, chr8a, chr8b, chr11a, chr11b, chry, es_cell_id, centre_id, created_at, updated_at)
-    #  select  distribution_qc_five_prime_sr_pcr, distribution_qc_three_prime_sr_pcr, distribution_qc_karyotype_low, distribution_qc_karyotype_high,
-    #          distribution_qc_copy_number, distribution_qc_five_prime_lr_pcr, distribution_qc_three_prime_lr_pcr,
-    #          distribution_qc_thawing, distribution_qc_loa, distribution_qc_loxp, distribution_qc_lacz,
-    #          distribution_qc_chr1, distribution_qc_chr8a, distribution_qc_chr8b, distribution_qc_chr11a, distribution_qc_chr11b,
-    #          distribution_qc_chry, es_cells.id, centres.id, NOW(), NOW() from es_cells, centres
-    #"
-
     execute "
       insert into distribution_qcs (five_prime_sr_pcr, three_prime_sr_pcr, karyotype_low, karyotype_high, copy_number, five_prime_lr_pcr, three_prime_lr_pcr,
               thawing, loa, loxp, lacz, chr1, chr8a, chr8b, chr11a, chr11b, chry, es_cell_id, centre_id, created_at, updated_at)
@@ -91,8 +76,5 @@ class CreateDistributionQcs < ActiveRecord::Migration
 
   def self.down
     drop_table :distribution_qcs
-
-#    execute "delete from distribution_qcs"
-
   end
 end
