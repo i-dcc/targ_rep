beacdc6907cfe199702ee79f461f85752eed811e
Asfand Qazi <aq2@sanger.ac.uk>
2012-11-12 17:40:46 +0000

    Improve output of fix_bad_mgi_accession_ids.targ_rep script

--- script/fix_bad_mgi_accession_ids.targ_rep.rb
+++ script/fix_bad_mgi_accession_ids.targ_rep.rb
@@ -8,11 +8,11 @@ gene.search(:q => '*:*', :rows => 1000000).each do |d|
   gene_name_lookup[d['mgi_accession_id']] = d['marker_symbol']
 end
 
-puts "Genes: #{gene_name_lookup.keys.size}"
+puts "NOTICE: Genes: #{gene_name_lookup.keys.size}"
 
 bad_alleles = Allele.all.find_all {|a| gene_name_lookup[a.mgi_accession_id].blank? }
 
-puts "Bad alleles: #{bad_alleles.size}"
+puts "NOTICE: Bad alleles: #{bad_alleles.size}"
 
 DCC_BIOMART = Biomart::Dataset.new(
   'http://www.i-dcc.org/biomart',
@@ -26,24 +26,32 @@ biomart_results = DCC_BIOMART.search(
   :required_attributes => ['mgi_accession_id', 'secondary_mgi_accession_id']
 )
 
-old_to_new_mgi_id_mapping = {}
+old_to_new_mgi_id_mapping = ActiveSupport::OrderedHash.new
 biomart_results.each do |result|
-  old_to_new_mgi_id_mapping[result['secondary_mgi_accession_id']] = result['mgi_accession_id']
+  old_to_new_mgi_id_mapping[result['secondary_mgi_accession_id'].to_s] = result['mgi_accession_id'].to_s
 end
 
 alleles_without_new_mgi_id = []
 
 Allele.transaction do
   bad_alleles.each do |allele|
+    old_mgi_id = allele.mgi_accession_id
     new_mgi_id = old_to_new_mgi_id_mapping[allele.mgi_accession_id]
     if new_mgi_id.blank?
       alleles_without_new_mgi_id << allele
     else
-      puts "Changing MGI id of Allele #{allele.id} from #{allele.mgi_accession_id} to #{new_mgi_id}"
       allele.mgi_accession_id = new_mgi_id
       if ! allele.save
-        puts "CRITICAL: Validation failed for Allele #{allele.id}: #{allele.errors.full_messages}"
+        if allele.errors[:project_design_id].include? 'must have unique design features'
+          puts "NOTICE: Deleting Allele #{allele.id}(#{old_mgi_id}) since another one already exists with same design features with updated MGI ID of #{new_mgi_id}"
+          allele.destroy
+        else
+          puts "CRITICAL: Validation failed for Allele #{allele.id}: #{allele.errors.full_messages}"
+        end
+      else
+        puts "NOTICE: Changing MGI id of Allele #{allele.id} from #{allele.mgi_accession_id} to #{new_mgi_id}"
       end
+
     end
   end
 
@@ -51,7 +59,7 @@ Allele.transaction do
     puts "CRITICAL: No new MGI id found for #{allele.mgi_accession_id} (Allele #{allele.id})"
   end
 
-  puts "old -> new MGI id mappings written to file tmp/old_to_new_mgi_accession_id_mapping.#{Rails.env}.yaml"
+  puts "NOTICE: old -> new MGI id mappings written to file tmp/old_to_new_mgi_accession_id_mapping.#{Rails.env}.yaml"
 
   File.open("tmp/old_to_new_mgi_accession_id_mapping.#{Rails.env}.yaml", 'wb') do |file|
     file.puts old_to_new_mgi_id_mapping.to_yaml
